3d6c61278be292c1eff09c5ba7cd903833d9171c
John Howard <jhoward@microsoft.com>
2018-05-11 09:54:56 -0700

    Now vsmb
    Signed-off-by: John Howard <jhoward@microsoft.com>

--- vendor/github.com/Microsoft/hcsshim/vsmb_test.go
+++ vendor/github.com/Microsoft/hcsshim/vsmb_test.go
@@ -4,20 +4,21 @@ import (
 	"os"
 	"testing"
 
-	"github.com/sirupsen/logrus"
+	//	"github.com/sirupsen/logrus"
 )
 
 // TestWCOWVSMB tests adding/removing VSMB from a v2 Windows utility VM
 func TestWCOWVSMB(t *testing.T) {
 	v2uvm, v2uvmScratchDir := createv2WCOWUVM(t, layersNanoserver, "", nil)
 	defer os.RemoveAll(v2uvmScratchDir)
+	startContainer(t, v2uvm)
 	defer v2uvm.Terminate()
-	v2uvmc := v2uvm.(*container)
+	//v2uvmc := v2uvm.(*container)
 
 	dir := createTempDir(t)
 
-	for i := 0; i < 3; i++ {
-		guid, _ := AddVSMB(v2uvm, dir, VsmbFlagReadOnly|VsmbFlagPseudoOplocks|VsmbFlagTakeBackupPrivilege|VsmbFlagCacheIO|VsmbFlagShareRead)
-		logrus.Debugln(guid, v2uvmc.vsmbShares)
+	for i := 0; i < 1; i++ {
+		AddVSMB(v2uvm, dir, VsmbFlagReadOnly|VsmbFlagPseudoOplocks|VsmbFlagTakeBackupPrivilege|VsmbFlagCacheIO|VsmbFlagShareRead)
+		RemoveVSMB(v2uvm, dir)
 	}
 }
